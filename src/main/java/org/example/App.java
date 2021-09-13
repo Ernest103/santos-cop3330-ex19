8d28e61fe2f386af4f79e2bb37ddc3af33b92fc9
Ernest103 <ernest0399@icloud.com>
2021-09-13 12:08:17 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+        boolean flag;
+        double height, weight, BMI;
+
+        do {
+            flag = true;
+
+            try{
+                System.out.print("PLease input your weight(LBS): ");
+                weight = sc.nextDouble();
+
+                System.out.print("Please input your height(IN): ");
+                height = sc.nextDouble();
+
+                BMI = (weight / (height * height)) * 703;
+
+                if(BMI >= 18.5 && BMI <= 25)
+                {
+                    System.out.printf("Your BMI is %.1f.\n" +
+                            "You are within the ideal weight range.", BMI);
+                }
+                else if(BMI > 25)
+                    System.out.printf("Your BMI is %.1f.\n" +
+                            "You are overweight. You should see your doctor.\n", BMI);
+                else
+                    System.out.printf("Your BMI is %.1f.\n" +
+                            "You are underweight. You should see your doctor.\n", BMI);
+
+            }catch (Exception InputMismatchException){
+                flag = false;
+            }
+
+        }while(flag == false);
+    }
+}
